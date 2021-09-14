5c37fe643a2194b796647071bf17f8dec06668ea
vycezhong <izhongyuchen@gmail.com>
2021-09-14 10:18:37 +0000

    update

--- PyTorch/pipeline_resnet50/run.sh
+++ PyTorch/pipeline_resnet50/run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-NNODES=1
-NPROC_PER_NODE=2
-MASTER_IP=localhost
-MASTER_PORT=2021
+NNODES=2
+NPROC_PER_NODE=1
+MASTER_IP=172.30.2.12
 
 
-python -m torch.distributed.run \
+python3 -m torch.distributed.run \
 	--nnodes=$NNODES --nproc_per_node=$NPROC_PER_NODE \
 	--rdzv_id=1234 --rdzv_backend=c10d \
-	--rdzv_endpoint=$MASTER_IP:$MASTER_PORT \
+	--rdzv_endpoint=$MASTER_IP \
 	main.py
