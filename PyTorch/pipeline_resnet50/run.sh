26b1d2bf0e9d9bc3d05add66b1bb90ce8b85993d
vycezhong <izhongyuchen@gmail.com>
2021-09-21 12:12:13 +0800

    minimal code to reproduce the bug

--- PyTorch/pipeline_resnet50/run.sh
+++ PyTorch/pipeline_resnet50/run.sh
@@ -5,7 +5,6 @@ NPROC_PER_NODE=1
 MASTER_IP=10.28.1.27
 MASTER_PORT=1234
 
-export CUDA_VISIBLE_DEVICES=1
 export NCCL_SOCKET_IFNAME=enp94s0
 
 python3 -m torch.distributed.run \
@@ -13,9 +12,6 @@ python3 -m torch.distributed.run \
 	--rdzv_id=1234 --rdzv_backend=c10d \
 	--rdzv_endpoint=$MASTER_IP \
 	main.py \
-	--micro-batch-size 64 \
-	--global-batch-size 256 \
-	--seed 2021 \
 	--master_ip $MASTER_IP \
 	--master_port $MASTER_PORT \
 	~/data/ILSVRC2012
