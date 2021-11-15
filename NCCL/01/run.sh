c91ce0d4b5425cb2d93824f808c09fea0ba197d2
vycezhong <izhongyuchen@gmail.com>
2021-11-15 12:10:38 +0800

    disable IB

--- NCCL/01/run.sh
+++ NCCL/01/run.sh
@@ -5,6 +5,7 @@
 export LD_LIBRARY_PATH=$LD_LDBRARY_PATH:/usr/local/cuda/lib 
 export NCCL_SOCKET_IFNAME=eth2
 export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
 
 # use absolute path
-mpirun -np 4 --hosts 10.28.1.16,10.28.1.17 /home/yczhong/repos/cs-notes/NCCL/01/test -x LD_LIBRARY_PATH -x NCCL_DEBUG -x NCCL_SOCKET_IFNAME
+mpirun -np 4 --hosts 10.28.1.16,10.28.1.17 /home/yczhong/repos/cs-notes/NCCL/01/test -x LD_LIBRARY_PATH -x NCCL_DEBUG -x NCCL_SOCKET_IFNAME -x NCCL_IB_DISABLE
