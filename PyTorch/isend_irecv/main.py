330eb19ae3c631155eb580b136d541e636b2dd46
Yuchen Zhong <izhongyuchen@gmail.com>
2021-11-25 19:25:40 +0800

    add Pytorch isend irecv (#31)
    * add isend irecv example
    
    * fix a bug
    
    * fix a bug
    
    * update script
    
    * update script
    
    * update script
    
    * update script
    
    * update

---
+++ PyTorch/isend_irecv/main.py
@@ -0,0 +1,38 @@
+import os
+from datetime import timedelta
+
+import torch
+
+
+def main():
+    local_rank = int(os.environ['LOCAL_RANK'])
+    torch.cuda.set_device(local_rank)
+    torch.distributed.init_process_group(
+        'nccl', init_method="env://",
+        timeout=timedelta(seconds=5)
+    )
+
+    rank = torch.distributed.get_rank()
+    size = (1000000, )
+    for _ in range(100):
+        dst_rank = (rank + 1) % 2
+        x = torch.ones(size=size, requires_grad=True).cuda()
+        y = torch.zeros(size=size, requires_grad=True).cuda()
+        send_op = torch.distributed.P2POp(torch.distributed.isend, x,
+                                          dst_rank)
+        recv_op = torch.distributed.P2POp(torch.distributed.irecv, y,
+                                          dst_rank)
+        reqs = torch.distributed.batch_isend_irecv([send_op, recv_op])
+        for req in reqs:
+            req.wait()
+
+        torch.cuda.synchronize()
+
+        z = x + y
+        assert z[0].item() == 2, "wrong"
+
+    print("right")
+
+
+if __name__ == '__main__':
+    main()
