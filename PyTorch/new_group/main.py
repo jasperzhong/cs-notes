99ab820cc1b92a51f24d064a80cdda632d902ecb
vycezhong <izhongyuchen@gmail.com>
2021-12-23 09:42:44 +0000

    update

--- PyTorch/new_group/main.py
+++ PyTorch/new_group/main.py
@@ -17,11 +17,12 @@ def main():
     else:
         new_group = torch.distributed.new_group([r for r in range(world_size) if r % 2 == 1])
 
-    new_rank = torch.distributed.get_rank(new_group)
-    print(f"{rank} -> {new_rank}")
+    group_size = torch.distributed.get_world_size(new_group)
+    group_rank = torch.distributed.get_rank(new_group)
+    print(f"{rank}/{world_size} -> {group_rank}/{group_size}")
 
     x = torch.randn(100).cuda()
-    torch.distributed.all_reduce(x, group=new_group)
+    new_group.all_reduce(x)
     print(f"{rank} {torch.sum(x)}")
 
 
