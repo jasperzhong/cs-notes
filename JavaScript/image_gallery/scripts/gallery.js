5fdfd617dc8214674180141bb92edf657d37cfc0
yuchenzhong <izhongyuchen@gmail.com>
2022-07-27 21:00:10 +0800

    update

--- JavaScript/image_gallery/scripts/gallery.js
+++ JavaScript/image_gallery/scripts/gallery.js
@@ -1,9 +1,22 @@
+window.onload = prepareLinks;
+function prepareLinks() {
+    var links = document.getElementsByClassName("gallery");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].className == "gallery") {
+            links[i].onclick = function () {
+                showPic(this);
+                return false;
+            }
+        }
+    }
+}
+
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
 
-    var text = whichpic.getAttribute("title");
+    var text = whichpic.firstChild.nodeValue;
     var caption = document.getElementById("caption");
     caption.firstChild.nodeValue = text;
 }
