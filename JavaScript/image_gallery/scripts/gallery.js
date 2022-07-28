17659644598bc5cd0351aec1d15c488153c35961
yuchenzhong <izhongyuchen@gmail.com>
2022-07-28 20:52:55 +0800

    sec 6

--- JavaScript/image_gallery/scripts/gallery.js
+++ JavaScript/image_gallery/scripts/gallery.js
@@ -1,22 +1,41 @@
-window.onload = prepareLinks;
-function prepareLinks() {
-    var links = document.getElementsByClassName("gallery");
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].className == "gallery") {
-            links[i].onclick = function () {
-                showPic(this);
-                return false;
-            }
+
+function addLoadEvent(func) {
+    var oldOnLoad = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldOnLoad();
+            func();
         }
     }
 }
 
+function prepareLinks() {
+    var links = document.getElementById("imagegallery").getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].tagName == "A") {
+            links[i].onclick = function () {
+                return !showPic(this);
+            }
+    }
+}
+
+addLoadEvent(prepareLinks);
+
 function showPic(whichpic) {
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
+    if (!placeholder) {
+        return false;
+    }
     placeholder.setAttribute("src", source);
 
     var text = whichpic.firstChild.nodeValue;
     var caption = document.getElementById("caption");
+    if (!caption) {
+        return false;
+    }
     caption.firstChild.nodeValue = text;
+    return true;
 }
